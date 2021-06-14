4c059b21e4c3a5bca3df46f2c9fc5a401c43cd93
松田彩 <moyashipoid@gmail.com>
2021-06-14 06:33:50 +0000

    プロフィール

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -1,13 +1,13 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        
-        <title>test</title>
-    </head>
-    <body>
-        <h1>profile</h1>
-    </body>
-</html>
+
+
+@extends('layouts.profile')
+
+@section('title', 'Myプロフィール')
+
+@section('content')
+    <div class="container">
+        <div class="col-md-8 mx-auto">
+            <h2>Myプロフィール</h2>
+        </div>
+    </div>
+@endsection
