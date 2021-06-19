b9a431bc8ff62558708ac64b8384bb95bbe0ea2f
松田彩 <moyashipoid@gmail.com>
2021-06-19 07:26:07 +0000

    編集実装

--- resources/views/admin/news/index.blade.php
+++ resources/views/admin/news/index.blade.php
@@ -11,7 +11,7 @@
                 <a href="{{ action('Admin\NewsController@add') }}" role="button" class="btn btn-primary">新規作成</a>
             </div>
             <div class="col-md-8">
-                <form action="{{ action('Admin\NewsController@index') }}" method="grt">
+                <form action="{{ action('Admin\NewsController@index') }}" method="get">
                     <div class="form-group row">
                         <label class="col-md-2">タイトル</label>
                         <div class="col-md-8">
@@ -26,7 +26,7 @@
             </div>
         </div>
         <div class="row">
-            <div class="list-news col-md-12 mx-auto">
+            <div class="admin-news col-md-12 mx-auto">
                 <div class="row">
                     <table class="table table-dark">
                         <thead>
@@ -34,14 +34,20 @@
                                 <th width="10%">ID</th>
                                 <th width="20%">タイトル</th>
                                 <th width="50%">本文</th>
+                                <th width="10%">操作</th>
                             </tr>
                         </thead>
                     <tbody>
-                    @foreach($posts as$news)
+                    @foreach($posts as $news)
                     <tr>
                         <th>{{ $news->id }}</th>
-                        <td>{{ \Str::limit($news->title, 100)}}</td>
-                        <td>{{ \Str::limit($news->body, 250)}}</td>
+                        <td>{{ str_limit($news->title, 100) }}</td>
+                        <td>{{ str_limit($news->body, 250) }}</td>
+                        <td>
+                            <div>
+                                <a href="{{ action('Admin\NewsController@edit', ['id' => $news->id]) }}">編集</a>
+                            </div>
+                        </td>
                     </tr>
                     @endforeach
                     </tbody>
