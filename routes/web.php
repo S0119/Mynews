bb0836fecaf8133d6fe1b2310e0d4d2aee18b668
松田彩 <moyashipoid@gmail.com>
2021-06-26 11:41:20 +0000

    プロフィール編集実装

--- routes/web.php
+++ routes/web.php
@@ -24,15 +24,14 @@ Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
     Route::get('news/delete', 'Admin\NewsController@delete');
 });
 
-Route::get('XXX', 'AAAController@bbb');
 
 Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function () {
     Route::get('profile/create', 'Admin\ProfileController@add');
     Route::post('profile/create', 'Admin\ProfileController@create');
+    Route::get('profile', 'Admin\ProfileController@index');
     Route::get('profile/edit', 'Admin\ProfileController@edit');
-    Route::post('profile/update', 'Admin\ProfileController@update');
+    Route::post('profile/edit', 'Admin\ProfileController@update');
 });
-//abc
 
 Auth::routes();
 
